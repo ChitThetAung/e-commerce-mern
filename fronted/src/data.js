103a206aaa66ab5f0e1e217b578fe2dbe481e40c
fernando Aung <fernandoaung28@gmail.com>
2021-07-27 19:38:52 +0630

    share code on github

---
+++ fronted/src/data.js
@@ -0,0 +1,79 @@
+export const data = {
+  products: [
+    {
+      _id: '1',
+      category: 'Shirts',
+      name: 'Nike Slim Shirt',
+      price: 120,
+      image: '/images/p1.jpg',
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      _id: '2',
+      category: 'Shirts',
+      name: 'Lacoste free Slim Shirt',
+      price: 100,
+      image: '/images/p2.jpg',
+
+      brand: 'Lacoste',
+      rating: 4,
+      numReviews: 11,
+      description: 'high quality product',
+    },
+
+    {
+      _id: '3',
+      category: 'Shirts',
+      name: 'Puma Slim Shirt',
+      price: 130,
+      image: '/images/p3.jpg',
+
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 18,
+      description: 'high quality product',
+    },
+
+    {
+      _id: '4',
+      category: 'Pants',
+      name: 'Puma White pants',
+      price: 110,
+      image: '/images/p4.jpg',
+
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 16,
+      description: 'high quality product',
+    },
+
+    {
+      _id: '5',
+      category: 'Pants',
+      name: 'Adidas Long Pants ',
+      price: 150,
+      image: '/images/p5.jpg',
+
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 15,
+      description: 'high quality product',
+    },
+
+    {
+      _id: '6',
+      category: 'Pants',
+      name: 'Nike Slim Shirt',
+      price: 80,
+      image: '/images/p6.jpg',
+
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+  ],
+};
